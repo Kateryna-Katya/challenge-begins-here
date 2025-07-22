23a6d61883a780610a9c9ec1cd489ad587547e52
Kateryna <katyaukr2014@gmail.com>
2025-07-22 13:50:15 +0300

    add

--- src/js/active-link.js
+++ src/js/active-link.js
@@ -2,6 +2,13 @@ const links = document.querySelectorAll('.navigation-item');
 
 links.forEach(link => {
   link.addEventListener('click', function (e) {
+    const href = this.getAttribute('href');
+
+    if (href && href.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(href)?.scrollIntoView({ behavior: 'smooth' });
+    }
+
     links.forEach(l => l.classList.remove('active'));
     this.classList.add('active');
   });
